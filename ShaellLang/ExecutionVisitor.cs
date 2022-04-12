f58edeec4f1f33b5ff444a714de2534a9cfbd45b
KiraStaehr <74182118+KiraStaehr@users.noreply.github.com>
2022-04-12 13:43:14 +0200

    removed troll

--- ShaellLang/ExecutionVisitor.cs
+++ ShaellLang/ExecutionVisitor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
 using System.Linq;
-using System.Security.Cryptography;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
 
